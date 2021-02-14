ef859544330359fdf9470e9a274b7faad408ce7a
Lewis Njagi <lewismaybach@gmail.com>
2021-02-14 16:57:51 +0300

    create user class

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    """
+    Class that generates new intances of users
+    """
+    user_list = []
+
+    def __init__(self,first_name,last_name,password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.last_name = last_name
